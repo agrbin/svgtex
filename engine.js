5242b3313ad5accde242537f4ad3404753da3a77
physikerwelt <wiki@physikerwelt.de>
2013-10-04 17:40:37 +0000

    Appling the Mediawiki syle convention and error handling

--- engine.js
+++ engine.js
@@ -1,131 +1,138 @@
 // perfect singleton
 window.engine = (new (function() {
 
-  this.Q = MathJax.Hub.queue;
-  this.math = null;
-  this.buffer = [];
+	this.Q = MathJax.Hub.queue;
+	this.math = null;
+	this.buffer = [];
 
-  function toMathML(jax,callback) {
-	  var mml;
-	  try {
-		  mml = jax.root.toMathML("");
-	  } catch(err) {
-		  if (!err.restart) {throw err} // an actual error
-		  return MathJax.Callback.After([toMathML,jax,callback],err.restart);
-	  }
-	  MathJax.Callback(callback)(mml);
-  }
+	function toMathML(jax,callback) {
+		var mml;
+		try {
+			mml = jax.root.toMathML('');
+		} catch(err) {
+			if (!err.restart) {throw err;} // an actual error
+			return MathJax.Callback.After([toMathML,jax,callback],err.restart);
+		}
+		MathJax.Callback(callback)(mml);
+	}
 
-  // bind helper.
-  this.bind = function(method) {
-    var engine = this;
-    return function() {
-      return method.apply(engine, arguments);
-    };
-  };
+	// bind helper.
+	this.bind = function(method) {
+		var engine = this;
+		return function() {
+			return method.apply(engine, arguments);
+		};
+	};
 
-  // initialize Engine, after MathJax is loaded, this.math will
-  // point to our jax.
-  this._init = function() {
-    this.Q.Push(this.bind(function () {
-      this.math = MathJax.Hub.getAllJax("math")[0];
-      this._process_buffered();
-    }));
-  };
+	// initialize Engine, after MathJax is loaded, this.math will
+	// point to our jax.
+	this.init = function() {
+		this.Q.Push(this.bind(function () {
+			this.math = MathJax.Hub.getAllJax('math')[0];
+			this.processBuffered();
+		}));
+	};
 
-  // receives input latex string and invokes cb
-  // function with svg result.
-  this._process = function(latex, cb) {
-    this.Q.Push(["Text", this.math, latex]);
-    this.Q.Push(this.bind(function() {
-      // then, this toSVG call will invoke cb(result).
-      cb(document.getElementsByTagName("svg")[1].cloneNode(true));
-    }));
-  };
+	// receives input latex string and invokes cb
+	// function with svg result.
+	this.processCB = function(latex, cb) {
+		this.Q.Push(['Text', this.math, latex]);
+		this.Q.Push(this.bind(function() {
+			// then, this toSVG call will invoke cb(result).
+			cb(document.getElementsByTagName('svg')[1].cloneNode(true));
+		}));
+	};
 
-  // this is a helper for merge, who will want to decide
-  // whether something went wrong while rendering latex.
-  // the constant #C00 could be overriden by config!!
-  this._text_is_error = function(txt) {
-    return txt.getAttribute("fill") == "#C00" &&
-      txt.getAttribute("stroke") == "none";
-  };
+	// this is a helper for merge, who will want to decide
+	// whether something went wrong while rendering latex.
+	// the constant #C00 could be overriden by config!!
+	this.TextIsError = function(txt) {
+		return txt.getAttribute('fill') === '#C00' &&
+			txt.getAttribute('stroke') === 'none';
+	};
 
-  // mathjax keeps parts of SVG symbols in one hidden svg at
-  // the begining of the DOM, this function should take two
-  // SVGs and return one stand-alone svg which could be
-  // displayed like an image on some different page.
-  this._merge = function(svg) {
-    var origDefs = document.getElementById('MathJax_SVG_Hidden')
-      .nextSibling.childNodes[0];
-    var defs = origDefs.cloneNode(false);
+	// mathjax keeps parts of SVG symbols in one hidden svg at
+	// the begining of the DOM, this function should take two
+	// SVGs and return one stand-alone svg which could be
+	// displayed like an image on some different page.
+	this.merge = function(svg) {
+		var uses,
+			copied,
+			k,
+			id,
+			texts,
+			i,
+			tmpDiv,
+			defs = document.getElementById('MathJax_SVG_Hidden')
+			.nextSibling.childNodes[0].cloneNode(false);
 
-    // append shalow defs and change xmlns.
-    svg.insertBefore(defs, svg.childNodes[0]);
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+		// append shalow defs and change xmlns.
+		svg.insertBefore(defs, svg.childNodes[0]);
+		svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 
-    // clone and copy all used paths into local defs.
-    // xlink:href in uses FIX
-    var uses = svg.getElementsByTagName("use"),
+		// clone and copy all used paths into local defs.
+		// xlink:href in uses FIX
+		uses = svg.getElementsByTagName('use');
 		copied = {};
-    for (var k = 0; k < uses.length; ++k) {
-      var id = uses[k].getAttribute("href");
-	  if (id && copied[id]) {
-		  uses[k].setAttribute("xlink:href", id);
-		  // Already copied, skip
-		  continue;
-	  }
-      defs.appendChild(
-        document.getElementById(id.substr(1)).cloneNode(true)
-      );
-      uses[k].setAttribute("xlink:href", id);
-	  copied[id] = true;
-    }
+		for ( k = 0; k < uses.length; ++k) {
+			id = uses[k].getAttribute('href');
+		if (id && copied[id]) {
+			uses[k].setAttribute('xlink:href', id);
+			// Already copied, skip
+			continue;
+		}
+			defs.appendChild(
+				document.getElementById(id.substr(1)).cloneNode(true)
+			);
+			uses[k].setAttribute('xlink:href', id);
+		copied[id] = true;
+		}
 
-    // check for errors in svg.
-    var texts = document.getElementsByTagName("text", svg);
-    for (var i = 0; i < texts.length; ++i) {
-      if (this._text_is_error(texts[i])) {
-        return [texts[i].textContent];
-      }
-    }
+		// check for errors in svg.
+		texts = document.getElementsByTagName('text', svg);
+		for ( i = 0; i < texts.length; ++i) {
+			if (this.TextIsError(texts[i])) {
+				return [texts[i].textContent];
+			}
+		}
 
-    svg.style.position = "static";
-    var tmpDiv = document.createElement('div');
-    tmpDiv.appendChild(svg);
-    return tmpDiv.innerHTML;
-  };
+		svg.style.position = 'static';
+		tmpDiv = document.createElement('div');
+		tmpDiv.appendChild(svg);
+		return tmpDiv.innerHTML;
+	};
 
-  // if someone calls process before init is complete,
-  // that call will be stored into buffer. After the init
-  // is complete, all buffer stuff will get resolved.
-  this._process_buffered = function() {
-    for (var i = 0; i < this.buffer.length; ++i) {
-      this.process(this.buffer[i][0], this.buffer[i][1]);
-    }
-    this.buffer = [];
-  };
+	// if someone calls process before init is complete,
+	// that call will be stored into buffer. After the init
+	// is complete, all buffer stuff will get resolved.
+	this.processBuffered = function() {
+		for (var i = 0; i < this.buffer.length; ++i) {
+			this.process(this.buffer[i][0], this.buffer[i][1]);
+		}
+		this.buffer = [];
+	};
 
-  // callback will be invoked with array [original latex, SVG output]
-  // if there is an error during the latex rendering then second
-  // element (instead of SVG output) will be array again with
-  // only one string element describing the error message.
-  this.process = function(latex, cb) {
-    if (this.math === null) {
-      this.buffer.push( [latex, cb] );
-    } else {
-      this._process(latex, this.bind(function(svg) {
-		  var jax = MathJax.Hub.getAllJax();
-		  var mergedSVG = this._merge(document.getElementsByTagName("svg")[1].cloneNode(true));
-		  toMathML(jax[0],function (mml) {
-			  mml = mml;
-			  //console.log(jax[0].originalText + "\n\n=>\n\n"+ mml);
-				cb([latex, mergedSVG, mml]);
-		  });
-		  //console.log(mergedSVG);
-      }));
-    }
-  };
+	// callback will be invoked with array [original latex, SVG output]
+	// if there is an error during the latex rendering then second
+	// element (instead of SVG output) will be array again with
+	// only one string element describing the error message.
+	this.process = function(latex, cb) {
+		if (this.math === null) {
+			this.buffer.push( [latex, cb] );
+		} else {
+			try{
+				this.processCB(latex, this.bind(function( ) {
+					var jax = MathJax.Hub.getAllJax(),
+						mergedSVG = this.merge(document.getElementsByTagName('svg')[1].cloneNode(true));
+					toMathML(jax[0],function (mml) {
+						cb([latex, mergedSVG, mml]);
+					});
+				}));
+			} catch (err) {
+				cb([latex, err.message, '-']);
+			}
+		}
+	};
 
-  this._init();
-}));
+	this.init();
+})());
